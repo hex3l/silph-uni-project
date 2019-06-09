5e34af7744b57e2addf110aef43a8654d14e597b
Hex3l <me@hex3l.net>
2019-06-09 22:44:35 +0200

    fixes maven tests

--- src/test/java/net/he3l/silph/SilphApplicationTests.java
+++ src/test/java/net/he3l/silph/SilphApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import net.hex3l.silph.SilphApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SilphApplication.class)
 public class SilphApplicationTests {
 
 	@Test
