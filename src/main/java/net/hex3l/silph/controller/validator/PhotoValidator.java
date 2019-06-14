bd75ee39ad7703ee76bb5fdba25d422b9a85ba30
Hexel <hdhexel@gmail.com>
2019-06-15 01:32:51 +0200

    implements LOB for 'Photo' + upload, confirmation pages

--- src/main/java/net/hex3l/silph/controller/validator/PhotoValidator.java
+++ src/main/java/net/hex3l/silph/controller/validator/PhotoValidator.java
@@ -17,9 +17,7 @@ public class PhotoValidator implements Validator {
 	
 	@Override
 	public void validate(Object target, Errors errors) {
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "albumid", "required");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "imageid", "required");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "imageext", "required"); 
-		//Validate album and image as numbers and imageext as png/jpeg
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "extension", "required");
 	}
 }
--- src/main/java/net/hex3l/silph/model/data/Photo.java
+++ src/main/java/net/hex3l/silph/model/data/Photo.java
@@ -1,11 +1,6 @@
 package net.hex3l.silph.model.data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -20,10 +15,10 @@ public class Photo {
 	private String name;
 	@Getter @Setter
 	private String extension;
-	//temporary using base64 (gonna use @Lob byte[] to store images)
 	@Getter @Setter
-	@Column(columnDefinition = "TEXT")
-	private String image;
+	@Lob
+	@Basic(fetch = FetchType.LAZY)
+	private byte[] image;
 	@Getter @Setter
 	@ManyToOne
 	private Album album;
