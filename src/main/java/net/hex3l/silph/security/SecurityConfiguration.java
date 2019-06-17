b2c8ddb1b94ee5ac8a0c93b9cd8930f6f6d7c16f
Hex3l <me@hex3l.net>
2019-06-17 17:12:41 +0200

    adds overridable security properties

--- src/main/java/net/hex3l/silph/security/SecurityConfiguration.java
+++ src/main/java/net/hex3l/silph/security/SecurityConfiguration.java
@@ -1,25 +1,27 @@
 package net.hex3l.silph.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private Environment env;
+	 
 	@Override
 	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
+			.withUser(env.getProperty("spring.security.user.name")).password(passwordEncoder()
+					.encode(env.getProperty("spring.security.user.password"))).roles("ADMIN");
 	}
 	
 	
