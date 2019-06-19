2c5d90f49edd51d73090291719fffd27de1dd5bf
Hex3l <me@hex3l.net>
2019-06-19 12:11:42 +0200

    adds cart in all pages

--- src/main/java/net/hex3l/silph/controller/SystemController.java
+++ src/main/java/net/hex3l/silph/controller/SystemController.java
@@ -1,15 +1,25 @@
 package net.hex3l.silph.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import net.hex3l.silph.services.CartService;
 
 @Controller
 public class SystemController{
 	
+	@Autowired
+	private CartService cartService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String displayPhotos() {
-
-		return "index";
+	public ModelAndView displayPhotos(HttpSession session) {
+		ModelAndView mav = new ModelAndView("index");
+		
+		return cartService.photoSelection(mav, session);
 	}
 }
